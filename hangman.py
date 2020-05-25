31539a990f1805a513076bb9b169695af1a23f90
Yana <51543583+yanastasevich@users.noreply.github.com>
2020-05-25 10:03:32 +0200

    Second game update
    - added the comparison of the user input and random word by symbol
    - added a start() method - the main method of the game

--- hangman.py
+++ hangman.py
@@ -1,53 +1,58 @@
 import random
 
 play_words = {
-    ' polycarbonate',
-    ' cat',
-    ' sugar',
-    ' junk',
-    ' stuff',
-    ' princess',
-    ' death'
+    'polycarbonate',
+    'cat',
+    'sugar',
+    'junk',
+    'stuff',
+    'princess',
+    'death'
 }
 
 
 random_word =  random.choice(tuple (play_words))
-print (random_word)
 
 
 def print_lanes(word):
     word_length = len (word);
-    print (' _ ' * word_length)
+    return ' _ ' * word_length
 
 
-def word_guess(g_word, r_word):
-    g_pos = 0
-    r_pos = 0
-    g_word = word_input (random_word)
-    while g_pos < len (g_word) and r_pos < len (r_word):
-          if g_word[g_pos] == r_word [r_pos]:
-              g_pos += 1
-          r_pos += 1
-    return g_pos == len (g_word)
-
-
+# the guess_count should be fixed
+# a separate mathod for it makes sense
+# a method that checks for the guess counts
+# i have to think about it tonight
 def input_check_word(word):
-    count = 0
+    guess_pos = 0
     guess_word = ' '
-    while count < len (word):
-        char_guess = input ("> ")
-        guess_word += char_guess
-        count += 1
-    else:
-        if guess_word == word:
-            print ("You guessed the whole word")
-        elif guess_word != word:
-            print ("Oops, you're hanged")
+    guess_count = 0
+    while guess_pos < len (word) and guess_count <= 3:
+        for t in word:
+            char_guess = input ("> ")
+            if char_guess == t:
+                guess_word += char_guess
+                guess_pos += 1
+                guess_count += 1
+
+        else:
+            print ("Oops, you're hanged.\n")
+            print ("Start again!\n")
+            start()
+    print ("Congratulations! You've guessed the whole letter!")
     return guess_word
 
 
+def start():
+    print ("""Welcome to Hangman!
+A game where you have to guess the letters of the word!
+You have three attempts to guess.\n""")
+
+    print (f"The random word today is {random_word}")
+    print (f"{print_lanes(random_word)} \n")
+    print (f"Let's start with the guessing!")
+    print (input_check_word(random_word))
 
 
-print_lanes (random_word)
-#print (word_guess (word_input, random_word))
-print (input_check_word (random_word))
+
+start()
