aec71de6ff3cb35d73da470264237805d7e2ec35
Yana <51543583+yanastasevich@users.noreply.github.com>
2020-05-26 22:44:38 +0200

    update #3
    refactored a big method
    added characters to the list
    added another way of checking for characters

--- hangman.py
+++ hangman.py
@@ -15,32 +15,26 @@ random_word =  random.choice(tuple (play_words))
 
 
 def print_lanes(word):
-    word_length = len (word);
-    return ' _ ' * word_length
+    lanes = []
+    word_length = len (word)
+    for i in range (len (word)):
+        lanes.extend ("_")
+    print (*lanes, sep = " ")
+    return lanes
 
 
-# the guess_count should be fixed
-# a separate mathod for it makes sense
-# a method that checks for the guess counts
-# i have to think about it tonight
-def input_check_word(word):
-    guess_pos = 0
-    guess_word = ' '
-    guess_count = 0
-    while guess_pos < len (word) and guess_count <= 3:
-        for t in word:
-            char_guess = input ("> ")
-            if char_guess == t:
-                guess_word += char_guess
-                guess_pos += 1
-                guess_count += 1
+def input_char(word):
+     char_guess = input ("> ")
+     return char_guess
 
-        else:
-            print ("Oops, you're hanged.\n")
-            print ("Start again!\n")
-            start()
-    print ("Congratulations! You've guessed the whole letter!")
-    return guess_word
+
+def check_char(word):
+    char_input = input_char(random_word)
+
+    if char_input in word:
+        return True
+    else:
+        return False
 
 
 def start():
@@ -48,10 +42,10 @@ def start():
 A game where you have to guess the letters of the word!
 You have three attempts to guess.\n""")
 
-    print (f"The random word today is {random_word}")
-    print (f"{print_lanes(random_word)} \n")
+    print (f"The random word today is {random_word}\n")
+    print_lanes(random_word)
     print (f"Let's start with the guessing!")
-    print (input_check_word(random_word))
+    print (check_char(random_word))
 
 
 
